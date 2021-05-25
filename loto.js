21386086a3eaaeb6955a928d9b95735356942a14
Mordasan Alexandru-Ioan <mordasanalex@yahoo.com>
2021-05-25 18:14:31 +0300

    update js and collors

--- loto.js
+++ loto.js
@@ -1,6 +1,6 @@
 //Etapa1:numeerele de la user
-let numbers = prompt("Enter the numbers:");
-numbers = stringToArray(numbers);
+// let numbers = prompt("Enter the numbers:");
+// numbers = stringToArray(numbers);
 
 
 
@@ -12,28 +12,27 @@ let generate = lottoNumbers();
 //conditii de existenta
 let rewrite = document.querySelector('.rewrite');
 let intro = document.querySelector('.intro');
+intro.innerHTML = displayIntro();
 
-if (verifyNumbers(numbers) == false || verify49(numbers) == false || verifyLength(numbers) == false) {
-    rewrite.innerHTML = displayRewrite();
-} else {
-    intro.innerHTML = displayIntro();
-}
 
 //verificare castig
 let proba = [1, 2, 3, 4, 5, 6];
-let numerePrinse = isWin(numbers, generate);
 let but = document.querySelector('#buton');
 let win = document.querySelector('.win');
 let afterclick = document.querySelector('.intro');
 
 but.addEventListener('click', () => {
+    let numbers = prompt("Enter the numbers:");
+    numbers = stringToArray(numbers);
+    let numerePrinse = isWin(numbers, generate);
     console.log(numerePrinse);
-    if (numerePrinse.length > 0) {
+    afterclick.innerHTML = displayAfterClick();
+    if (verifyNumbers(numbers) == false || verify49(numbers) == false || verifyLength(numbers) == false) {
+        rewrite.innerHTML = displayRewrite();
+    } else if (numerePrinse.length > 0) {
         cont.innerHTML = displayNumbersBox(generate);
         win.innerHTML = displayWin(numerePrinse);
-        afterclick.innerHTML = displayAfterClick();
     } else {
-        afterclick.innerHTML = displayAfterClick();
         win.innerHTML = displayLose();
     }
 })
